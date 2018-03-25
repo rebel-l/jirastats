e2dbb6b0d58ff1023307476b989a7a2a416699e2
Lars Gaubisch <lars.gaubisch@home24.de>
2018-03-25 18:16:44 +0200

    added TODO

--- packages/database/table.go
+++ packages/database/table.go
@@ -8,4 +8,5 @@ type Table interface {
 	CreateStructure() error
 	Select(where string, args ...interface{}) (rows *sql.Rows, err error)
 	Truncate() error
+	// TODO: SelectComplex(where string, order string, fields string, group string, args ...interface{})
 }
