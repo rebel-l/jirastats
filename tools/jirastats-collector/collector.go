9a319f6944446c8eff1c69fb2ab80831fcee88c3
Lars Gaubisch <lars.gaubisch@home24.de>
2018-03-25 18:49:58 +0200

    removed dead code

--- tools/jirastats-collector/collector.go
+++ tools/jirastats-collector/collector.go
@@ -14,9 +14,6 @@ import (
 	"time"
 )
 
-const StatementInsertTicket = "INSERT INTO ticket(`key`) values(?)" // TODO: deprecated
-const StatementSelectTickets = "SELECT * FROM ticket" // TODO: deprecated
-
 
 func main() {
 	verbose := utils.GetVerboseFlag()
@@ -46,12 +43,7 @@ func main() {
 		pp.Process()
 	}
 
-
-	//
-	//tickets := getTickets()
-	//storeTickets(tickets, db)
-	//readTickets(db)
-
+	// calculate elapsed time
 	t := time.Now()
 	elapsed := t.Sub(c.start)
 	log.Infof("Processed %d projects in %f seconds", len(projects), elapsed.Seconds())
@@ -79,46 +71,4 @@ func (c *Collector) getProjects() (projects []*models.Project, err error) {
 func (c *Collector) getJiraClient() *jira.Client {
 	config := jp.NewConfig(c.db)
 	return jp.NewClient(config)
-}
-
-
-
-func storeTickets(tickets []jira.Issue, db *sql.DB) {
-	// TODO: deprecated
-	stmt, err := db.Prepare(StatementInsertTicket)
-	handleUnrecoverableError(err)
-
-	for _, v := range tickets {
-		_, err = stmt.Exec(v.Key)
-		if err != nil {
-			log.Warnf("Couldn't save ticket %s: %s", v.Key, err.Error())
-		}
-	}
-}
-
-func readTickets(db *sql.DB) {
-	// TODO: deprecated
-	stmt, err := db.Prepare(StatementSelectTickets)
-	handleUnrecoverableError(err)
-	rows, err := stmt.Query()
-	if err != nil {
-		log.Warn("Couldn't read tickets from database")
-		return
-	}
-
-	var id int
-	var key string
-
-	for rows.Next() {
-		err = rows.Scan(&id, &key)
-		log.Infof("TicketId: %d with Key: %s", id, key)
-	}
-}
-
-func handleUnrecoverableError(err error) {
-	// TODO: deprecated
-	if err != nil {
-		log.Errorf("Unrecoverable error appeard: %s", err.Error())
-		log.Panic("Collector failed ... Goodbye!")
-	}
 }
