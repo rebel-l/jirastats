268571ec12d6b55222d390875c61ceadc789eb9a
Lars Gaubisch <lars.gaubisch@home24.de>
2018-05-13 03:36:13 +0200

    ensured that data is loaded for comparison only on changes of the parameters

--- client/components/charts/ComparisonGroup.js
+++ client/components/charts/ComparisonGroup.js
@@ -12,20 +12,43 @@ const mapStateToProps = state => {
 class ComparisonGroupComp extends Component{
     constructor(props){
         super(props);
+        this.countPropsDates = 0;
+        this.dataRecieved = false;
         this.state = {
-            projectId: props.projectId
+            projectId: props.projectId,
+            data: null
         }
     }
 
-    componentDidUpdate(){
-        let dates = this.getComparisonSelectorDates();
-        if(dates.left === "" || dates.right === ""){
-            return;
+    shouldComponentUpdate(nextProps, nextState){
+        let lastDates = nextProps.dates[nextProps.dates.length - 1];
+        if(lastDates.left === "" || lastDates.right === ""){
+            return false;
         }
 
+        if(nextProps.dates.length === this.countPropsDates){
+            if(this.dataRecieved === false && nextState.data !== null) {
+                this.dataRecieved = true;
+                return true;
+            }
+            return false;
+        } else {
+            this.dataRecieved = false;
+            this.countPropsDates = nextProps.dates.length;
+        }
+
+        return true;
+    }
+
+    componentDidUpdate(){
+        if(this.dataRecieved === true){
+            return;
+        }
+        let dates = this.getComparisonSelectorDates();
         axios.get(`/data/stats/comparison/${this.state.projectId}/${dates.left}/${dates.right}`).then(res => {
-            // TODO: send data to chart compnent
-            console.log(res.data);
+            let newState = Object.assign({}, this.state);
+            newState.data = res.data;
+            this.setState(newState);
         });
     }
 
@@ -34,8 +57,14 @@ class ComparisonGroupComp extends Component{
     }
 
     render(){
+        if(this.state.data === null){
+            return(
+                <p>Select the dates for the two versions of project {this.state.projectId}.</p>
+            );
+        }
+
         return(
-            <p>Select the dates for the two versions of project {this.state.projectId}.</p>
+          <p>Data received</p>
         );
     }
 }
