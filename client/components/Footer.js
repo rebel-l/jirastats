6249172a6df667f4de3dda264fe5cbf983056040
Rebel-L <dj@rebel-l.net>
2018-05-27 19:52:42 +0200

    added css modules and styled footer

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,10 +1,13 @@
 // Framework
 import React, {Component} from "react";
 
+// Style
+import style from "./Footer.css";
+
 class Footer extends Component {
     constructor(props){
         super(props);
-        this.licenceLink = "https://www.gnu.org/licenses/gpl-3.0";
+        this.licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
         this.profileLink = "https://www.linkedin.com/in/lars-gaubisch-a6b1a423/";
     }
 
@@ -20,18 +23,16 @@ class Footer extends Component {
 
     render(){
         return(
-            <div key={"footer"} id={"footer"}>
+            <div key={"footer"} id={"footer"} className={style.footer}>
                 <p>
                     Copyright © {Footer.getYear()} by <a href={this.profileLink}>Lars Gaubisch</a>
                 </p>
                 <p>
-                    Published under <a href={this.licenceLink}>GNU GPL.3.0</a>
+                    Licensed under <a href={this.licenseLink}>GNU GPL.3.0</a>
                 </p>
             </div>
         );
     }
 }
-
-
 
 export default Footer;
--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,24 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
             }
         ]
     },
