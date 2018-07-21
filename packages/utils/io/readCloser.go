ff9d00a49c1d9929c9e6184763b600a412ba7a3c
Rebel-L <dj@rebel-l.net>
2018-07-21 22:03:14 +0200

    added a conversion from io.ReadCloser to []byte

--- packages/utils/io/readCloser.go
+++ packages/utils/io/readCloser.go
@@ -5,14 +5,29 @@ import (
 	"io"
 )
 
+// ReadCloserToString turns the stream into a string
 func ReadCloserToString(rc io.ReadCloser) (res string, err error) {
-	buf := new(bytes.Buffer)
-	_, err = buf.ReadFrom(rc)
+	buf, err := readCloserToBuffer(rc)
 	if err != nil {
 		return
 	}
-	defer rc.Close()
-
 	res = buf.String()
+	return
+}
+
+//ReadCloserToByte turns the stream into a byte array
+func ReadCloserToByte(rc io.ReadCloser) (res []byte, err error) {
+	buf, err := readCloserToBuffer(rc)
+	if err != nil {
+		return
+	}
+	res = buf.Bytes()
+	return
+}
+
+func readCloserToBuffer(rc io.ReadCloser) (buf *bytes.Buffer, err error) {
+	buf = new(bytes.Buffer)
+	_, err = buf.ReadFrom(rc)
+	defer rc.Close()
 	return
 }
